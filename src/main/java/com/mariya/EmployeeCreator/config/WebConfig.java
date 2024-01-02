e1fae2e184353b7d7680ba5d90b5000a963f2e19
Mariya Abdul Ghafoor <mariya.aghafoor@gmail.com>
2024-01-02 11:24:22 +1100

    adds allowed origins for cors

--- src/main/java/com/mariya/EmployeeCreator/config/WebConfig.java
+++ src/main/java/com/mariya/EmployeeCreator/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
   public void addCorsMappings(CorsRegistry registry) {
 
-    String[] allowedOrigins = { "http://localhost:5173/", "http://127.0.0.1:5173/", "http://localhost:5174/" };
+    String[] allowedOrigins = { "http://localhost:5173/", "http://127.0.0.1:5173/", "http://localhost:5174/","employee-creator.netlify.app" };
 
     registry.addMapping("/**")
         .allowedOrigins(allowedOrigins)
